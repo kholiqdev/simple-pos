c77ba90c7ac768ff054d97c11145cdb464dbd4df
Kholiq <kholiqcode@gmail.com>
2023-04-29 19:15:10 +0700

    chore: setup lintstaged config

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const buildEslintCommand = filenames =>
+  `eslint --fix ${filenames
+    .map(f => path.relative(process.cwd(), f))
+    .join(' --fix ')}`;
+
+module.exports = {
+  '*.{json}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
+};
