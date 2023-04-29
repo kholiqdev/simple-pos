c5c312f20ba95ce62f9b0ad916385e487a4e8771
Kholiq <kholiqcode@gmail.com>
2023-04-29 22:18:33 +0700

    feat: create base splash screen

---
+++ src/features/shared/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function SplashScreen(): JSX.Element {
+  return (
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text>SplashScreen</Text>
+    </View>
+  );
+}
