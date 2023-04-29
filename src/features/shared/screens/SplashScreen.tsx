a1e1490b039b6cfc63b0722f138f4332ff1c5148
Kholiq <kholiqcode@gmail.com>
2023-04-30 01:18:30 +0700

    feat: implement restyle custom ui

--- src/features/shared/screens/SplashScreen.tsx
+++ src/features/shared/screens/SplashScreen.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
 import {t as _} from 'i18next';
 
+import {Box, Text} from '@components/atoms';
+
 export default function SplashScreen(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text style={styles.appTitle}>{_('point_of_sale')}</Text>
-    </View>
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Text variant="header" color="textPrimaryColor">
+        {_('point_of_sale')}
+      </Text>
+    </Box>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  appTitle: {color: 'black', fontSize: 32, fontWeight: '600'},
-});
