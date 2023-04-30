21a97aba01d188efe00818dabbe6fa4ad84599bb
Kholiq <kholiqcode@gmail.com>
2023-04-30 18:39:00 +0700

    feat: create input atom component

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {TextInput, type TextInputProps} from 'react-native';
+
+import {useRestyle, useTheme} from '@shopify/restyle';
+
+import {HStack} from '@components/atoms/HStack';
+import {restyleFunctions, type RestyleProps, type Theme} from '@theme/theme';
+
+type InputProps = {
+  leftAddon?: string | JSX.Element;
+  rightAddon?: string | JSX.Element;
+} & RestyleProps &
+  TextInputProps;
+
+export default function Input(props: InputProps): JSX.Element {
+  const {leftAddon, rightAddon, ...baseProps} = useRestyle(
+    restyleFunctions,
+    props,
+  );
+  const theme = useTheme<Theme>();
+
+  return (
+    <HStack
+      style={{borderRadius: 15}}
+      backgroundColor="cardSecondaryBackground"
+      alignItems="center"
+      px="xs">
+      {leftAddon}
+      <TextInput
+        placeholderTextColor={theme.colors.textSecondaryColor}
+        {...baseProps}
+        style={{
+          flex: 1,
+          color: theme.colors.textPrimaryColor,
+        }}
+        cursorColor={theme.colors.textSecondaryColor}
+      />
+      {rightAddon}
+    </HStack>
+  );
+}
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,6 @@
 export * from '@components/atoms/Box';
 export * from '@components/atoms/HStack';
 export * from '@components/atoms/Icon';
+export * from '@components/atoms/Input';
 export * from '@components/atoms/Text';
 export * from '@components/atoms/VStack';
