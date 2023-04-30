adbaae7e8d6cc7db9f11510a2e0a609aefb956ac
Kholiq <kholiqcode@gmail.com>
2023-04-30 19:26:38 +0700

    feat: create category badge component

---
+++ src/features/sales/components/CategoryBadge.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+
+import {useTheme} from '@shopify/restyle';
+
+import {Box, IconMaterial} from '@components/atoms';
+import {type BoxProps} from '@components/atoms/Box/Box';
+import {type Theme} from '@theme/theme';
+
+type CategoryBadgeProps = {
+  onPress: () => void;
+  iconName: string;
+  isSelected?: boolean;
+} & BoxProps;
+
+export default React.memo(function CategoryBadge(
+  props: CategoryBadgeProps,
+): JSX.Element {
+  const {onPress, iconName, isSelected = false, ...baseProps} = props;
+
+  const theme = useTheme<Theme>();
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Box
+        borderWidth={isSelected ? 1 : 0}
+        backgroundColor="cardSecondaryBackground"
+        px="m"
+        py="xs"
+        borderRadius="xl"
+        {...baseProps}>
+        <IconMaterial
+          name={iconName}
+          size={24}
+          color={
+            isSelected
+              ? theme.colors.textPrimaryColor
+              : theme.colors.textSecondaryColor
+          }
+        />
+      </Box>
+    </TouchableOpacity>
+  );
+});
