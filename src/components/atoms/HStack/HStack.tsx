9d6b8eaa0e6203a3f2d03def650358efc0acdf2e
Kholiq <kholiqcode@gmail.com>
2023-04-30 17:36:45 +0700

    feat: can export Hstack props

--- src/components/atoms/HStack/HStack.tsx
+++ src/components/atoms/HStack/HStack.tsx
@@ -7,7 +7,7 @@ import {type Theme} from '@theme/theme';
 
 const Box = createBox<Theme>();
 
-type HStackProps = BoxProps<Theme> & ViewProps;
+export type HStackProps = BoxProps<Theme> & ViewProps;
 
 export default React.memo(function HStack(props: HStackProps) {
   const {children, ...baseProps} = props;
