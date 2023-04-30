9f4980e78c8ef3e558a435a11f69bea11ebe46aa
Kholiq <kholiqcode@gmail.com>
2023-05-01 06:23:32 +0700

    feat: add onpress in floating order button

--- src/features/sales/components/FloatingOrderButton.tsx
+++ src/features/sales/components/FloatingOrderButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Pressable} from 'react-native';
 
 import {Text} from '@components/atoms';
 import HStack, {type HStackProps} from '@components/atoms/HStack/HStack';
@@ -8,28 +9,31 @@ import {verticalScale} from '@utils/layout';
 type FloatingOrderButtonProps = {
   title: string;
   price: number;
+  onPress?: () => void;
 } & HStackProps;
 
 export default React.memo(function FloatingOrderButton(
   props: FloatingOrderButtonProps,
 ): JSX.Element {
-  const {title, price, ...baseProps} = props;
+  const {title, price, onPress, ...baseProps} = props;
 
   return (
-    <HStack
-      my="xs"
-      backgroundColor="buttonPrimaryBackground"
-      justifyContent="space-between"
-      px="xl"
-      alignItems="center"
-      style={{paddingVertical: verticalScale(15), borderRadius: 20}}
-      {...baseProps}>
-      <Text color="textTertiaryColor" fontSize={16} fontWeight="bold">
-        {title}
-      </Text>
-      <Text color="textTertiaryColor" fontSize={16} fontWeight="bold">
-        {formatCurrency(price)}
-      </Text>
-    </HStack>
+    <Pressable onPress={onPress}>
+      <HStack
+        my="xs"
+        backgroundColor="buttonPrimaryBackground"
+        justifyContent="space-between"
+        px="xl"
+        alignItems="center"
+        style={{paddingVertical: verticalScale(15), borderRadius: 20}}
+        {...baseProps}>
+        <Text color="textTertiaryColor" fontSize={16} fontWeight="bold">
+          {title}
+        </Text>
+        <Text color="textTertiaryColor" fontSize={16} fontWeight="bold">
+          {formatCurrency(price)}
+        </Text>
+      </HStack>
+    </Pressable>
   );
 });
