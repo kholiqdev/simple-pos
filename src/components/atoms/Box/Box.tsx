627dbe0f5b4fe4c02df11512a5e00985f5f71515
Kholiq <kholiqcode@gmail.com>
2023-04-30 17:05:19 +0700

    fix: change type name box props

--- src/components/atoms/Box/Box.tsx
+++ src/components/atoms/Box/Box.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {type ViewProps} from 'react-native/types';
 
-import {type BoxProps, createBox} from '@shopify/restyle';
+import {type BoxProps as RBoxProps, createBox} from '@shopify/restyle';
 
 import {type Theme} from '@theme/theme';
 
 const BaseBox = createBox<Theme>();
 
-type BaseBoxProps = BoxProps<Theme> & ViewProps;
+export type BoxProps = RBoxProps<Theme> & ViewProps;
 
-export default React.memo(function Box(props: BaseBoxProps) {
+export default React.memo(function Box(props: BoxProps) {
   const {children, ...baseProps} = props;
 
   return <BaseBox {...baseProps}>{children}</BaseBox>;
