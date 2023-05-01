289fcd45e247e580f000206b1e822863f29ade62
Kholiq <kholiqcode@gmail.com>
2023-05-01 09:31:15 +0700

    chore: install react native bluetooh printer

--- android/app/src/main/java/com/simplepos/MainApplication.java
+++ android/app/src/main/java/com/simplepos/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import cn.jystudio.bluetooth.RNBluetoothEscposPrinterPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
